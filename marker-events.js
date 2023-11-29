e2d332ebc255d7f4e546b760f41672df034bff43
chcollins <chcollins@uri.edu>
2023-11-29 11:25:18 -0500

    gesture and handler

--- marker-events.js
+++
@@ -1,9 +0,0 @@
-const sceneEl = document.querySelector('scene');
-
-sceneEl.addEventListener("markerFound", (e) => {
-    isMarkerVisible = true;
-});
-
-sceneEl.addEventListener("markerLost", (e) => {
-    isMarkerVisible = false;
-});
